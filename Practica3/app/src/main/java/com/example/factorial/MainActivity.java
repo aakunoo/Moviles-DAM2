f626d2d90ecfbdcce6145e0ac2c5caaafaa2f7bd
Jerónimo Vicente <jerovicvid@gmail.com>
2024-09-26 14:16:31 +0200

    CalculoFactorialv0
    Sin terminar

---
+++ Practica3/app/src/main/java/com/example/factorial/MainActivity.java
@@ -0,0 +1,61 @@
+package com.example.factorial;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    EditText numtxt;
+    Button boton1;
+    TextView resultfact;
+
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+
+        });
+        numtxt = findViewById(R.id.numerito);
+        boton1 = findViewById(R.id.bt1);
+        resultfact = findViewById(R.id.txtfactorial);
+
+        boton1.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
+
+    public void Factorizar(){
+        int numero = Integer.parseInt(numtxt.getText().toString());
+        int resultf=1;
+
+        if(numero>0){
+            for (int i = 0; i <=numero; i++) {
+                // Sin terminar
+            }
+
+        }
+    }
+
+
+
+}
